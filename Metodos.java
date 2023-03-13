398ed114fa9154c680574822f07db1ca668b61ec
Karla Ivonne Morlan <108318500+KarlaMorlan@users.noreply.github.com>
2023-03-13 11:55:13 -0600

    Add files via upload

---
+++ Metodos.java
@@ -0,0 +1,92 @@
+
+package Calificaciones.Principal;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author kaivo
+ */
+public class Metodos {
+        String nombre;
+        int calif[] = new int[5];
+        int suma=0;
+        float promedio=0;
+        
+        // Ingresa nombre del alumno
+        public void ingresaNombre(){
+            
+            Scanner n = new Scanner (System.in);
+            System.out.println("Ingresa el nombre del alumno:  ");
+            nombre = n.nextLine();
+
+        }
+        
+        // Ingresar 5 calificaciones
+        public void calPromedio(){
+
+            Scanner c=new Scanner(System.in);
+            System.out.println("Ingresa las 5 calificaciones del alumno:");
+
+             for (int i=0; i<calif.length; i++) {
+                 calif[i]=c.nextInt();
+                 suma=suma+calif[i];
+            }
+             for (int i=0; i<calif.length; i++) {
+                 System.out.println(calif[i]);
+            }
+                 
+            // Promedio del alumno
+
+            promedio=(suma/calif.length);      
+       
+        }
+        
+        //Asignar nota segun el promedio final
+        public float notaFinal() {
+        
+            if ((promedio>=91) && (promedio<=100))
+            {
+               System.out.println("Calificación: A");
+            }
+            else if ((promedio>=81) && (promedio<=90))
+            {
+                System.out.println("Calificación: B");
+            }
+            else if ((promedio>=71) && (promedio<=80))
+            {
+                System.out.println("Calificación: C");
+            }
+            else if ((promedio>=61) && (promedio<=70))
+            {
+                System.out.println("Calificación: D");
+            }
+            else if ((promedio>=51) && (promedio<=60))
+            {
+                System.out.println("Calificación: E");
+            }
+            else if (promedio<=50)
+            {
+                System.out.println("Calificación: F");
+            }
+            return promedio;
+          
+        }
+        
+        public void Imprime(){
+            
+            // Imprime titulo
+            System.out.print("CALIFICACIONES UNIVERSIDAD TECMILENIO\n");
+            // Imprime nombre del alumno
+            System.out.println("Nombre del alumno: " + nombre);
+            // Imprime las calificaciones del alumno
+            System.out.println("Calificación 1 = " + calif[0]);
+            System.out.println("Calificación 2 = " + calif[1]);
+            System.out.println("Calificación 3 = " + calif[2]);
+            System.out.println("Calificación 4 = " + calif[3]);
+            System.out.println("Calificación 5 = " + calif[4]);
+            //Imprime el resultado del promedio
+            System.out.println("El promedio es:" + promedio);   
+        }
+    
+}
---
+++ Principal.java
@@ -0,0 +1,23 @@
+
+package Calificaciones.Principal;
+
+/**
+ *
+ * @author kaivo
+ */
+public class Principal {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+               
+        Metodos obj=new Metodos();
+        obj.ingresaNombre();
+        obj.calPromedio();
+        obj.notaFinal();
+        obj.Imprime();
+    }
+    
+    
+    
+}
